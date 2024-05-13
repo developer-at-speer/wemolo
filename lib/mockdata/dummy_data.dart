6483dd2450687391537512dff9035cdfc1ffc5bb
developer@speer <developer@speer.io>
2024-05-13 10:38:58 -0400

    add mock data

---
+++ lib/mockdata/dummy_data.dart
@@ -0,0 +1,109 @@
+import 'package:parking/models/parking_lot.dart';
+
+  List<ParkingLot> parkingLots1 = [
+    ParkingLot(
+      address: "Verdistrasse 13, 81257 München",
+      id: "51cd3ad8-58b4-4a13-8a98-6ebcd338789e",
+      imageURL: "https://media.istockphoto.com/id/1294474797/de/foto/luftaufnahme-des-parkplatzes-mit-verschiedenen-farbigen-fahrzeugen.jpg?s=2048x2048&w=is&k=20&c=Zh1g9L112Luu-tm4zFxsSEbjCPE7qCgmOGvJnR2eLHU=",
+      liveDate: "2020-02-21",
+      name: "Lot Munich 13",
+      size: 25,
+      status: "active",
+      type: "store"
+    ),
+    ParkingLot(
+      address: "Verdistrasse 19, 81257 München",
+      id: "5596e0f1-4d2f-4793-a848-b64d60e57f41",
+      imageURL: "https://images.freeimages.com/images/large-previews/e8e/underground-parking-1206464.jpg",
+      liveDate: "2020-05-07",
+      name: "Lot Munich 19",
+      size: 25,
+      status: "active",
+      type: "store"
+    ),
+    ParkingLot(
+      address: "Amalienburgstr. 10, 81247 München",
+      id: "5675a03c-9235-4f70-9e72-6c28f2fcd205",
+      imageURL: "https://images.freeimages.com/images/large-previews/68d/narrow-street-1204865.jpg",
+      liveDate: "2020-01-01",
+      name: "Lot Munich 10",
+      size: 25,
+      status: "active",
+      type: "hotel"
+    ),
+    ParkingLot(
+      address: "Verdistrasse 37, 81257 München",
+      id: "5bb38e41-837e-4808-bfba-1836e4ead49d",
+      imageURL: "https://images.freeimages.com/images/large-previews/47f/parking-space-1441053.jpg",
+      liveDate: null,
+      name: "Lot Munich 37",
+      size: 25,
+      status: "active",
+      type: "store"
+    ),
+    ParkingLot(
+      address: "Verdistrasse 9, 81257 München",
+      id: "5faca454-7ba0-47e6-84a4-7d0551ad9ac2",
+      imageURL: "https://images.freeimages.com/images/large-previews/e8e/underground-parking-1206464.jpg",
+      liveDate: "2020-01-01",
+      name: "Lot Munich 9",
+      size: 25,
+      status: "active",
+      type: "store"
+    )
+  ];
+
+    List<ParkingLot> parkingLots2 = [
+    ParkingLot(
+      address: "Verdistrasse 17, 81257 München",
+      id: "61bf6c3b-1785-4ea1-9cc3-10a7e8f6c226",
+      imageURL: "https://images.freeimages.com/images/large-previews/47f/parking-space-1441053.jpg",
+      liveDate: "2020-04-05",
+      name: "Lot Munich 17",
+      size: 25,
+      status: "active",
+      type: "store"
+    ),
+    ParkingLot(
+      address: "Amalienburgstr. 200, 81247 München",
+      id: "63827315-0949-4a71-80d2-593cd1e2d6a0",
+      imageURL: "https://images.freeimages.com/images/large-previews/68d/narrow-street-1204865.jpg",
+      liveDate: "2020-05-20",
+      name: "Lot Munich 20",
+      size: 25,
+      status: "active",
+      type: "hotel"
+    ),
+    ParkingLot(
+      address: "Verdistrasse 12, 81257 München",
+      id: "67a6a7f4-1c67-4a07-9048-05d25b9846d2",
+      imageURL: "https://images.freeimages.com/images/large-previews/47f/parking-space-1441053.jpg",
+      liveDate: "2020-01-01",
+      name: "Lot Munich 12",
+      size: 25,
+      status: "active",
+      type: "store"
+    ),
+    ParkingLot(
+      address: "Random Str. 27, 00001 RandomVille",
+      id: "6a288453-605e-44dd-b330-ca0f94f4be9c",
+      imageURL: "https://images.freeimages.com/images/large-previews/68d/narrow-street-1204865.jpg",
+      liveDate: "2020-01-01",
+      name: "Lot Munich 5",
+      size: 25,
+      status: "active",
+      type: "hotel"
+    ),
+    ParkingLot(
+      address: "Verdistrasse 203, 81257 München",
+      id: "6a84b67a-bd72-411e-a132-b3f0ee54d3ba",
+      imageURL: "https://media.istockphoto.com/id/1294474797/de/foto/luftaufnahme-des-parkplatzes-mit-verschiedenen-farbigen-fahrzeugen.jpg?s=2048x2048&w=is&k=20&c=Zh1g9L112Luu-tm4zFxsSEbjCPE7qCgmOGvJnR2eLHU=",
+      liveDate: "2020-09-01",
+      name: "Lot Munich 23",
+      size: 25,
+      status: "active",
+      type: "store"
+    ),
+  ];
+
+
